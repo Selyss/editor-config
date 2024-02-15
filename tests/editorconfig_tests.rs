a3aaf67a196503fd46d18d96d3beb67b958e8ae0
Selyss <99344963+Selyss@users.noreply.github.com>
2024-02-14 21:34:29 -0500

    example test

--- tests/editorconfig_tests.rs
+++ tests/editorconfig_tests.rs
@@ -41,4 +41,24 @@ mod tests {
             Some(&String::from("space"))
         );
     }
+
+    #[test]
+    fn test_example_file_parse() {
+        let editorconfig_path = "tests/test_data/.editorconfig_example";
+        let editorconfig = EditorConfig::from_file(editorconfig_path).unwrap();
+
+        assert_eq!(editorconfig.config.len(), 6);
+        assert_eq!(
+            editorconfig.get_property("*", "end_of_line"),
+            Some(&String::from("lf"))
+        );
+        assert_eq!(
+            editorconfig.get_property("*", "insert_final_newline"),
+            Some(&String::from("true"))
+        );
+        assert_eq!(
+            editorconfig.get_property("*.js", "indent_style"),
+            Some(&String::from("space"))
+        );
+    }
 }
