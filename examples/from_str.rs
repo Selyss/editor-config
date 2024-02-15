ea76b46d0b115b900c25cd26ab967d8653104213
Selyss <99344963+Selyss@users.noreply.github.com>
2024-02-15 12:31:11 -0500

    another example

---
+++ examples/from_str.rs
@@ -0,0 +1,22 @@
+//! An example of getting an editorconfig from a string
+
+use editor_config::parser::EditorConfig;
+
+fn main() {
+    let editorconfig_str = r#"
+        [*]
+        indent_style = tab
+        indent_size = 4
+
+        [*.js]
+        indent_style = space
+    "#;
+
+    let editorconfig: EditorConfig = editorconfig_str.parse().unwrap();
+
+    if let Some(indent_style) = editorconfig.get_property("*.js", "indent_style") {
+        println!("Indent Style: {}", indent_style);
+    } else {
+        println!("Indent style not specified.");
+    }
+}
