0fcc2a856ae920d0e86918769e0c5763fc4a446f
Selyss <99344963+Selyss@users.noreply.github.com>
2024-02-14 20:23:10 -0500

    add from string

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 use ini::Ini;
 use std::collections::HashMap;
 use std::fs;
+use std::str::FromStr;
 
 pub struct EditorConfig {
     config: HashMap<String, HashMap<String, String>>,
@@ -9,16 +10,24 @@ pub struct EditorConfig {
 impl EditorConfig {
     pub fn from_file(file_path: &str) -> Result<Self, String> {
         let contents = fs::read_to_string(file_path).map_err(|e| e.to_string())?;
-        let parsed = Ini::load_from_str(&contents).map_err(|e| e.to_string())?;
+        EditorConfig::from_str(&contents)
+    }
+}
+
+impl FromStr for EditorConfig {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parsed = Ini::load_from_str(s).map_err(|e| e.to_string())?;
         let config = parsed
             .iter()
             .map(|(section, properties)| {
-                let name = section.expect("Error: no section name").to_string();
-                let properties = properties
+                let section_name = section.expect("Error: cannot parse section").to_string();
+                let section_props = properties
                     .iter()
                     .map(|(key, value)| (key.to_string(), value.to_string()))
                     .collect();
-                (name, properties)
+                (section_name, section_props)
             })
             .collect();
 
