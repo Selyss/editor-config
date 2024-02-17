af1fe4ffa67c8cafdda50c27e42b0893c7b5bee3
Selyss <99344963+Selyss@users.noreply.github.com>
2024-02-17 18:32:49 -0500

    update main page docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,21 @@
 //! An [editorconfig]-parsing library
 //!
-//! Editorconfig is a standardized configuration file for text editors
+//! This crate serves as a thin wrapper around the editorconfig specification.
 //!
+//! An example of getting an editorconfig property
+//! ```rust
+//! let editorconfig_path = "tests/test_data/.editorconfig";
 //!
-//! ```toml
-//! [*]
-//! indent_style = tab
-//! indent_size = 4
+//! let editorconfig = EditorConfig::from_file(editorconfig_path).unwrap();
 //!
-//! [*.js]
-//! indent_style = space
+//! if let Some(indent_style) = editorconfig.get_property("*", "end_of_line") {
+//!     println!("EOL: {}", indent_style);
+//! } else {
+//!     println!("End of line not specified.");
+//! }
 //! ```
 //!
-//! ## Parsing editorconfig
+//!
 //! This crate only parses and returns the values, it does not impose any implementation details on the editor
 //!
 //! The file must be provided to the parser, it does not search the filesystem.
